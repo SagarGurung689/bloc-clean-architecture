66f4992cacfa4f649f714fe342519d76ea10d91a
wow finstack <wowfinstack@wows-Mac-mini.local>
2025-09-27 16:25:26 +0545

    bloc with freeze

--- lib/bloc_state_feature/presentation/bloc/get_products_bloc/get_products_bloc.dart
+++ lib/bloc_state_feature/presentation/bloc/get_products_bloc/get_products_bloc.dart
@@ -1,21 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:bloc2/bloc_state_feature/domain/entity/get_product_response.dart';
 import 'package:bloc2/bloc_state_feature/domain/repository/product_repository.dart';
-import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'get_products_bloc_event.dart';
 part 'get_products_bloc_state.dart';
+part 'get_products_bloc.freezed.dart';
 
-class GetProductsBlocBloc extends Bloc<GetProductsBlocEvent, GetProductsBlocState> {
+class GetProductsBloc extends Bloc<GetProductsEvent, GetProductsState> {
   final ProductRepository productRepository;
-  GetProductsBlocBloc(this.productRepository) : super(GetProductsBlocInitial()) {
-    on<GetProductsEvent>((event, emit)  async {
-      emit(GetProductsBlocLoading());
+  GetProductsBloc(this.productRepository) : super(GetProductsState.initial()) {
+    on<_FetchProducts>((event, emit)  async {
+      emit(GetProductsState.loading());
       var response = await productRepository.getProducts();
       if(response.statusCode == 200){
-        emit(GetProductsBlocLoaded(response.data));
+        emit(GetProductsState.loaded(response.data));
       } else {
-        emit(GetProductsBlocError(response.message ?? ''));
+        emit(GetProductsState.error(response.message ?? ''));
       }
       
     });
